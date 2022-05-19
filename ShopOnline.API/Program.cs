2b87b11db8238d1cab7f1a58f68213379be7c3ed
Mert <CChristofear@hotmail.com>
2022-05-19 19:26:33 +0300

    added images, added card view components and product page

--- ShopOnline.API/Program.cs
+++ ShopOnline.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using ShopOnline.API.Data;
 using ShopOnline.API.Repositories;
 using ShopOnline.API.Repositories.Contracts;
@@ -25,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(
+    policy => policy.WithOrigins("http://localhost:7202", "https://localhost:7202")
+    .AllowAnyMethod()
+    .WithHeaders(HeaderNames.ContentType)
+    );
 
 app.UseHttpsRedirection();
 
